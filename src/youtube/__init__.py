16e57e36f636e7340caa7ff504a6eab97ae771ff
azullimaodrive-droid <azullimaodrive@gmail.com>
2025-12-14 15:56:42 -0300

    Create YouTube module initialization file

---
+++ src/youtube/__init__.py
@@ -0,0 +1,11 @@
+"""YouTube module for Jingletube.
+
+This module provides functionality for interacting with YouTube,
+including video fetching, metadata extraction, and streaming capabilities.
+"""
+
+__version__ = "0.1.0"
+__author__ = "azullimaodrive-droid"
+
+# Export main components
+__all__ = []
